6baf6717f9a5db7ca3327791ff541584adb0aaed
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-04-14 16:30:43 +0300

    sum of two numbers

---
+++ test/testfile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y, sum;
+
+	sum = x + y;
+	printf("Enter the first number: ");
+	scanf("%d", &x);
+	printf("Enter the second number: ");
+	scanf("%d", &y);
+	return (0);
+}
